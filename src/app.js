9718b35ad3775d7315744d9ffea4b76f06cb3f4a
Jonathan Aldas <jonaaldas@gmail.com>
2022-07-20 18:12:18 -0500

    re-ordered files

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import { gettingData } from "./getData.js"
+import { gettingData } from "./utils/getData.js"
 // IIFE (immediately invoked function expression)
 // Runs the code as soon as its define  
 (async () => {
